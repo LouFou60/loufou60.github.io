6580084efec8f8289afa181bc2548ebcc8ac6b21
LouFou60 <loufou60@gmail.com>
2023-01-25 20:23:27 +0100

    Commit

--- js/testingAPI.js
+++ js/testingAPI.js
@@ -2,6 +2,17 @@ $(".panel").html("The script `testingAPI.js` is working! The API is loading . . 
 
 const teamsID = ['15165', '15170', '15173', '15188', '15201', '15204', '15206', '15217', '15219', '15233', '15234', '15247', '15252', '15263', '15269', '15270', '15287', '15295', '15308', '15341']
 
+function createDivTeam(teamData){
+    var div = document.createElement("div");
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.background = "red";
+    div.style.color = "white";
+    div.innerHTML = teamData.name;
+
+    document.getElementById("mainTeam").appendChild(div);
+}
+
 $.getJSON('../overwatchLeague.json', function(data){
     console.log('Inside the function')
 
@@ -11,6 +22,6 @@ $.getJSON('../overwatchLeague.json', function(data){
     // }
 
     for(var key in data.teams){
-        console.log(data.teams[key])
+        createDivTeam(data.teams[key])
     }
 })
