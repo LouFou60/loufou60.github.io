b8e91951b336c247e4f8c90798cba1e5a63d892c
LouFou60 <loufou60@gmail.com>
2023-01-25 18:12:50 +0100

    Update testingAPI.js

--- js/testingAPI.js
+++ js/testingAPI.js
@@ -2,10 +2,11 @@ $(".panel").html("The script `testingAPI.js` is working! The API is loading . . 
 
 const teamsID = ['15165', '15170', '15173', '15188', '15201', '15204', '15206', '15217', '15219', '15233', '15234', '15247', '15252', '15263', '15269', '15270', '15287', '15295', '15308', '15341']
 
-console.log('Outside the function')
 $.getJSON('https://us.api.blizzard.com/owl/v1/owl2?access_token=EUlyecleF2k3JU0x5CkqzEyh7A4drJkj8v', function(data){
     //This is the function
     console.log('Inside the function')
+
+    console.log(JSON.parse(data))
 });
 
 // for(i = 0; i < teamsID.length; i=i+1){
