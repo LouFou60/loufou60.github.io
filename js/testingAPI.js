bb20e2643db63a90f75e356adccc921d73a422d8
LouFou60 <loufou60@gmail.com>
2023-01-25 18:29:42 +0100

    Update testingAPI.js

--- js/testingAPI.js
+++ js/testingAPI.js
@@ -2,7 +2,7 @@ $(".panel").html("The script `testingAPI.js` is working! The API is loading . . 
 
 const teamsID = ['15165', '15170', '15173', '15188', '15201', '15204', '15206', '15217', '15219', '15233', '15234', '15247', '15252', '15263', '15269', '15270', '15287', '15295', '15308', '15341']
 
-$.getJSON('https://us.api.blizzard.com/owl/v1/owl2?access_token=EUlyecleF2k3JU0x5CkqzEyh7A4drJkj8v', function(data){
+$.getJSON('../overwatchLeague.json', function(data){
     //This is the function
     console.log('Inside the function')
 
